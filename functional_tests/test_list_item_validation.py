8f988aa92ad4a98b437c1df54c976d5927408292
cmermingas <cmermingas@gmail.com>
2016-05-08 23:10:11 -0400

    Enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -21,7 +21,7 @@ class ItemValidationTest(FunctionalTest):
         # The list display page shows the same error message
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # Finally, Carlos adds some text and enters another item
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
